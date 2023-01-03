153377032e61bd8f14211d6d38e48a6c3a0b9b36
MikeTribbiani <mishachaban8@gmail.com>
2023-01-03 14:36:41 +0200

    add new branch

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,1 +1,15 @@
 /// <reference types="Cypress" />
+
+//type
+it("type", () => {
+  cy.visit("https://next.privat24.ua/?lang=en")
+    .get('[data-qa-node="undefined-number"]')
+    .type(112233344);
+});
+
+//focus
+it("focus", () => {
+  cy.visit("https://next.privat24.ua/?lang=en")
+    .get('[data-qa-node="undefined-number"]')
+    .type(112233344);
+});
