c18a0a69685d527f77b52c42edde60f764e2ddc4
MikeTribbiani <mishachaban8@gmail.com>
2023-01-03 12:57:07 +0200

    change setup

--- cypress.config.js
+++ cypress.config.js
@@ -7,3 +7,11 @@ module.exports = defineConfig({
       ("**/1-getting-started/*.js", "**/2-advanced-examples/*.js"),
   },
 });
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    if (browser.name === "chrome") {
+      launchOptions.args.push("--disable-dev-shm-usage");
+    }
+    return launchOptions;
+  });
+};
